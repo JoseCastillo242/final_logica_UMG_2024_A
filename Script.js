66039a4506463e8bbb15a2f1fd993113624cf3cd
JoseCastillo242 <161986669+JoseCastillo242@users.noreply.github.com>
2024-06-08 11:00:39 -0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,19 @@
+function Funcion1(){
+    const Test1 = document.getElementById('Test1').value;
+    const Test2 = document.getElementById('Test2').value;
+    const Test3 = document.getElementById('Test3').value;
+    const resultado = document.getElementById('resultado').value;
+    if (Test1 < Test2) {
+        //Nms me muero perro lol esta es la suma xd
+        //resultado.textContent = Test1 + Test2 + Test3
+        console.log("Me cago en");
+    }
+    if (Test2 === 0) {
+        //Restar 
+        //resultado.textContent = Test1 + Test2 + Test3
+        console.log("Pos");
+    }
+}
+function Funcion2(){
+    
+}
